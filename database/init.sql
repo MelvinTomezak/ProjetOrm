f90c9072d2e00ad611d5daa69f9d1032235a1181
Melvin <melvin.tomezak@ynov.com>
2024-11-26 14:05:30 +0100

    Modification de Table News en news

--- database/init.sql
+++ database/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS ormdatabase;
 
 USE ormdatabase;
 
-CREATE TABLE IF NOT EXISTS `Table News` (
+CREATE TABLE IF NOT EXISTS `news` (
     id VARCHAR(36) NOT NULL PRIMARY KEY,
     content VARCHAR(256) NOT NULL,
     created_at DATETIME NOT NULL        
