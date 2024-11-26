2530dc4ca541532715538f130c57c4323056166a
Melvin <melvin.tomezak@ynov.com>
2024-11-26 12:53:17 +0100

    Ajout de la bd avec Docker

---
+++ database/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS ormdatabase;
+
+USE ormdatabase;
+
+CREATE TABLE IF NOT EXISTS `Table News` (
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    content VARCHAR(256) NOT NULL,
+    created_at DATETIME NOT NULL        
+);
