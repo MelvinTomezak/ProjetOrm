c4e0b88c2c9fea8234d5580f6f0b40cc124e28b5
Melvin <melvin.tomezak@ynov.com>
2024-11-26 14:31:57 +0100

    Insertion de données de test

--- database/init.sql
+++ database/init.sql
@@ -7,3 +7,10 @@ CREATE TABLE IF NOT EXISTS `news` (
     content VARCHAR(256) NOT NULL,
     created_at DATETIME NOT NULL        
 );
+
+-- Insertion de données de test
+
+INSERT INTO `news` (id, content, created_at) VALUES
+                                                       ('1', 'Premier article de nouvelles.', '2024-11-26 12:30:00'),
+                                                       ('2', 'Deuxième article de nouvelles.', '2024-11-26 12:35:00'),
+                                                       ('3', 'Troisième article de nouvelles.', '2024-11-26 12:40:00');
